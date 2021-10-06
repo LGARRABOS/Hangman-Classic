ef0cb7abf33baedf63f58cb75d34901bd78bf55e
AFERREIRA <alex.ferreira@ynov.com>
2021-10-06 11:44:57 +0200

    randomword

---
+++ random.go
@@ -0,0 +1,26 @@
+package piscine 
+
+import (
+	"bufio"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+func RandomWord() string {
+	rand.Seed(time.Now().UnixNano())
+	var wordtab []string
+	var word string
+	f, err := os.Open("word.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		wordtab = append(wordtab, scanner.Text())
+    }
+	word = wordtab[rand.Intn(len(wordtab))]
+	return word
+	
+}
