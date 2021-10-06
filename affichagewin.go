84dcf21eb9cbbbfc0df2b00a395e1daf0a6a40be
AFERREIRA <alex.ferreira@ynov.com>
2021-10-06 16:47:16 +0200

    congrat

--- affichagewin.go
+++ affichagewin.go
@@ -3,9 +3,12 @@ package piscine
 import "fmt"
 
 func Affichagefind(word string, letterchoose string, tabunderscore []rune) []rune {
-	fmt.Println(word)
 	var txtletter string
-	
+	if  tabunderscore[0] == 0 {
+		for i := 0; i < len(tabunderscore); i++ {
+			tabunderscore[i] = '_'
+		}
+	}
 	for i:= 0; i < len(tabunderscore); i++ {
 		if tabunderscore[i] <= 'z' && tabunderscore[i] >= 'a' {
 		} else if tabunderscore[i] == '_' && word[i] == letterchoose[0] {
