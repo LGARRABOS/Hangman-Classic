263f37e494e49c85456c3dc68b18374701f5c908
AFERREIRA <alex.ferreira@ynov.com>
2021-10-06 16:24:53 +0200

    affihage

--- hangman/main.go
+++ hangman/main.go
@@ -8,20 +8,32 @@ import (
 func main() {
 	attempts := 10
 	var choice string
+	c := 0
 	fmt.Println("Not present in the word, ", attempts, " attempts.")
 	word := piscine.RandomWord()
+	tabunderscore := make([]rune, len(word))
 	baseletter := piscine.LetterRandom(word)
-	piscine.Affichagefind(word, baseletter)
+	tabunderscore = piscine.Affichagefind(word, baseletter, tabunderscore)
 	for attempts > 0 {
 		choice = piscine.Choice()
+		c = 0
 		if piscine.AllVerif(choice) {
 			if piscine.Verif_letter_in_word(word, choice) {
-				piscine.Affichagefind(word, choice)
+				tabunderscore = piscine.Affichagefind(word, choice, tabunderscore)
+				for i := 0; i < len(word); i++ {
+					if rune(word[i]) == tabunderscore[i] {
+						c++
+					}
+				}
 			} else {
 				attempts--
 				piscine.PrintHangmanError(attempts)
 			}
 
+		}
+		if c == len(word) {
+		 fmt.Println("Congrats !")
+		 break
 		}
 	}
 	
