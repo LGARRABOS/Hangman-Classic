1369bb877a9847b7305eff52de5276bf736d3b53
LGARRABOS <garrabos.luc@ynov.com>
2021-10-06 14:20:25 +0200

    VOILA LA SA FONCTIONNE TT BIEN CORRECTEMENT

--- testeluc/main.go
+++ testeluc/main.go
@@ -8,23 +8,38 @@ import (
 func main() {
 	var stock []byte
 	word := "joseph"
-	if piscine.Verif_taille_osArgs() {
-		if piscine.Verif_taille_string() {
-			if piscine.Verif_letter() {
-				if piscine.Lettre_utiliser(&stock) {
-					if piscine.Verif_letter_in_word(word) {
-						fmt.Println("La lettre donnée se trouve dans le mot")
-					} else {
-						fmt.Println("La lettre donnée ne se trouve pas dans le mot")
-					}
+	l := "a"
+	if piscine.Verif_taille(l) {
+		if piscine.Verif_letter(l) {
+			if piscine.Lettre_utiliser(&stock, l) {
+				if piscine.Verif_letter_in_word(l, word) {
+					fmt.Println("La lettre donnée se trouve dans le mot")
 				} else {
-					fmt.Println("La lettre donnée à déja été proposer")
+					fmt.Println("La lettre donnée ne se trouve pas dans le mot")
 				}
 			} else {
-				fmt.Println("Votre arguments ne peux contenir qu'une lettre minuscule")
+				fmt.Println("La lettre donnée à déja été proposer")
 			}
 		} else {
-			fmt.Println("Votre arguments ne doit contenir qu'un seul carractére")
+			fmt.Println("Votre arguments ne peux contenir qu'une lettre minuscule")
+		}
+	} else {
+		fmt.Println("Vous ne pouvez envoyer qu'un seul argument")
+	}
+	l = "a"
+	if piscine.Verif_taille(l) {
+		if piscine.Verif_letter(l) {
+			if piscine.Lettre_utiliser(&stock, l) {
+				if piscine.Verif_letter_in_word(l, word) {
+					fmt.Println("La lettre donnée se trouve dans le mot")
+				} else {
+					fmt.Println("La lettre donnée ne se trouve pas dans le mot")
+				}
+			} else {
+				fmt.Println("La lettre donnée à déja été proposer")
+			}
+		} else {
+			fmt.Println("Votre arguments ne peux contenir qu'une lettre minuscule")
 		}
 	} else {
 		fmt.Println("Vous ne pouvez envoyer qu'un seul argument")
