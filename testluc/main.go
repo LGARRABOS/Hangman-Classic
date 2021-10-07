f24d2ab27c19eed638896b1debfc85facf183084
LGARRABOS <garrabos.luc@ynov.com>
2021-10-07 11:02:02 +0200

    alpahbet min

--- testluc/main.go
+++ testluc/main.go
@@ -9,6 +9,6 @@ func main() {
 	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 	for i := 0; i < len(alphabet); i++ {
 		g = alphabet[i]
-		piscine.Ascii_art(g)
+		piscine.Ascii_art_min(g)
 	}
 }
