4638ae1c9dcaac8ee5d3e3eee9b7504cf48836c3
AFERREIRA <alex.ferreira@ynov.com>
2021-10-07 18:39:27 +0200

    debut réparation lettre

--- hangman/main.go
+++ hangman/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"piscine"
 	"fmt"
-	"os"
 )
 
 func main() {
@@ -12,48 +11,44 @@ func main() {
 	c := 0
 	var stock []byte
 	fmt.Println("Good luck, you have", attempts, " attempts.")
-	if len(os.Args) == 2 {
-		word := piscine.RandomWord()
-		tabunderscore := make([]rune, len(word))
-		baseletter := piscine.LetterRandom(word)
-		tabunderscore = piscine.Affichagefind(word, baseletter, tabunderscore)
-		piscine.Ascii_art_min(tabunderscore)
-		for attempts > 0 {
-			choice = piscine.Choice()
-			c = 0
-			if piscine.AllVerif(choice, &stock, word) {
-				if piscine.Verif_letter_in_word(word, choice) {
-					tabunderscore = piscine.Affichagefind(word, choice, tabunderscore)
-					piscine.Ascii_art_min(tabunderscore)
-					for i := 0; i < len(word); i++ {
-						if rune(word[i]) == tabunderscore[i] {
-							c++
-						}
+	word := piscine.RandomWord()
+	tabunderscore := make([]rune, len(word))
+	baseletter := piscine.LetterRandom(word)
+	tabunderscore = piscine.Affichagefind(word, baseletter, tabunderscore)
+	piscine.LetterType(tabunderscore)
+	for attempts > 0 {
+		choice = piscine.Choice()
+		c = 0
+		if piscine.AllVerif(choice, &stock, word) {
+			if piscine.Verif_letter_in_word(word, choice) {
+				tabunderscore = piscine.Affichagefind(word, choice, tabunderscore)
+				piscine.LetterType(tabunderscore)
+				for i := 0; i < len(word); i++ {
+					if rune(word[i]) == tabunderscore[i] {
+						c++
 					}
-				} else {
-					attempts--
-					piscine.PrintHangmanError(attempts)
 				}
-				if c == len(word) {
-				 fmt.Println("Congrats !")
-				 break
+			} else {
+				attempts--
+				piscine.PrintHangmanError(attempts)
+			}
+			if c == len(word) {
+			 fmt.Println("Congrats !")
+			 break
+			}
+		} else if len(choice) == len(word) {
+			if piscine.Complet_word(word, choice) {
+				for i := 0; i < len(word); i++ {
+					tabunderscore[i] = rune(word[i])
 				}
-			} else if len(choice) == len(word) {
-				if piscine.Complet_word(word, choice) {
-					for i := 0; i < len(word); i++ {
-						tabunderscore[i] = rune(word[i])
-					}
-					piscine.Ascii_art_min(tabunderscore)
-					fmt.Println("\n")
-					fmt.Println("Congrats !")
-					break
-				} else {
-					attempts -= 2
-					piscine.PrintHangmanError(attempts)
-				}
+				piscine.LetterType(tabunderscore)
+				fmt.Println("\n")
+				fmt.Println("Congrats !")
+				break
+			} else {
+				attempts -= 2
+				piscine.PrintHangmanError(attempts)
 			}
 		}
-	} else {
-		fmt.Println("pas le bon nombre de fichier")
 	}
 }
