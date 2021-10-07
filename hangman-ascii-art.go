1805946f4500b16ecf62ac0e94c842945f4e66bb
JSELVA <joseph.selva@ynov.com>
2021-10-07 14:21:49 +0200

    affichage

--- hangman-ascii-art.go
+++ hangman-ascii-art.go
@@ -18,23 +18,25 @@ func Ascii_art_min(tabunderscore string) {
 	tabl6 := []string{"| (_| | ","| |_) | ","| (__  ","| (_| | ","|  __/ ","| |   ","  __/ | ","| | | | ","| | "," _/ | ","|   <   ","| | ","| | | | | | ","| | | | ","| (_) | ","| |     ","    | | ","| |    ","\\__ \\","\\ |_ ","| |_| | "," \\ V /  "," \\ V  V /    "," >  <    "," __/ /  "," / /  "," ______ "}
 	tabl7 := []string{" \\__,_|","|_.__/  "," \\___|"," \\__,_|"," \\___|","|_|   "," |___/  ","|_| |_| ","|_| ","|__/  ","|_|\\_\\","|_| ","|_| |_| |_| ","|_| |_| "," \\___/ ","|_|     ","    |_| ","|_|    ","|___/  "," \\__|"," \\__,_|","  \\_/   ","  \\_/\\_/    ","/_/\\_\\ ","|___/   ","/___| ","|______|"}
 
-	for i := 97; i <= 122; i++ {
-		if tabunderscore[i-97] == i {
-			ligne1 = ligne1 + tab1[i-97]
-			ligne2 = ligne2 + tab2[i-97]
-			ligne3 = ligne3 + tab3[i-97]
-			ligne4 = ligne4 + tab4[i-97]
-			ligne5 = ligne5 + tab5[i-97]
-			ligne6 = ligne6 + tab6[i-97]
-			ligne7 = ligne7 + tab7[i-97]
-		} else {
-			ligne1 = ligne1 + tab1[26]
-			ligne2 = ligne2 + tab2[26]
-			ligne3 = ligne3 + tab3[26]
-			ligne4 = ligne4 + tab4[26]
-			ligne5 = ligne5 + tab5[26]
-			ligne6 = ligne6 + tab6[26]
-			ligne7 = ligne7 + tab7[26]
+	for j := 0; j < len(tabunderscore); j++ {
+		for i := 97; i <= 122; i++ {
+			if tabunderscore[j] == i {
+				ligne1 = ligne1 + tab1[i-97]
+				ligne2 = ligne2 + tab2[i-97]
+				ligne3 = ligne3 + tab3[i-97]
+				ligne4 = ligne4 + tab4[i-97]
+				ligne5 = ligne5 + tab5[i-97]
+				ligne6 = ligne6 + tab6[i-97]
+				ligne7 = ligne7 + tab7[i-97]
+			} else if tabunderscore[] == '_' {
+				ligne1 = ligne1 + tab1[26]
+				ligne2 = ligne2 + tab2[26]
+				ligne3 = ligne3 + tab3[26]
+				ligne4 = ligne4 + tab4[26]
+				ligne5 = ligne5 + tab5[26]
+				ligne6 = ligne6 + tab6[26]
+				ligne7 = ligne7 + tab7[26]
+			}
 		}
 	}
 	fmt.Println(ligne1)
